f01a5de30dd82b27d2a3f4afeec958326ae0fba7
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-02-28 11:47:06 +0100

    Update CartItem to pass tests

--- src/CartItem.js
+++ src/CartItem.js
@@ -8,6 +8,10 @@
 
 "use strict";
 
+const InvalidArticleIdException = require("./exceptions/CartItemException.js");
+const InvalidQuantityException = require("./exceptions/CartItemException.js");
+const InvalidPriceException = require("./exceptions/CartItemException.js");
+
 module.exports = class CartItem {
 
     //region private attributes
@@ -27,6 +31,15 @@ module.exports = class CartItem {
      * @exception InvalidPriceException is thrown when the price is smaller than 10.
      */
     constructor(articleId, quantity, price) {
+        if (articleId < 1) {
+            throw new InvalidArticleIdException('The id cannot be smaller than one.');
+        }
+        if (quantity < 1) {
+            throw new InvalidQuantityException('The id cannot be smaller than one.');
+        }
+        if (price < 10){
+            throw  new InvalidPriceException('The id cannot be smaller than one.');
+        }
         this.#articleId = articleId;
         this.#quantity = quantity;
         this.#price = price;
@@ -51,7 +64,10 @@ module.exports = class CartItem {
      * @exception InvalidQuantityException is thrown when the quantity is smaller than 1.
      */
     set Quantity(value) {
-        throw new Error();
+        if (value < 1) {
+            throw new InvalidQuantityException('The quantity cannot be smaller than one.');
+        }
+        this.#quantity = value;
     }
 
     /**
@@ -67,7 +83,10 @@ module.exports = class CartItem {
      * @exception InvalidPriceException is thrown when the price is smaller than 10.
      */
     set Price(value) {
-        throw new Error();
+        if (value < 10) {
+            throw new InvalidPriceException('The price cannot be smaller than 10.');
+        }
+        this.#price = value;
     }
 
     /**
@@ -82,23 +101,7 @@ module.exports = class CartItem {
     //endregion private methods
 }
 
-class Error {
-    constructor(message) {
-        this.message = message;
-    }
-}
 
-class CartItemException extends Error{
-}
-
-module.exports = class InvalidArticleIdException extends CartItemException{
-}
-
-module.exports = class InvalidQuantityException extends CartItemException{
-}
-
-module.exports = class InvalidPriceException extends CartItemException{
-}
 
 
 
